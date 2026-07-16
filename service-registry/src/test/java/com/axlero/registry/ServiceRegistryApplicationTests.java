4d9e6145a7109843ce4aa3d0e680ac57d33abba1
N Pradeepa <pradeepansp295@gmail.com>
2026-07-16 23:37:35 +0530

    Initial setup: Eureka Service Registry

---
+++ service-registry/src/test/java/com/axlero/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axlero.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
