7852a3eb61a305b7d8cfd9948cc4610f0b19e639
N Pradeepa <pradeepansp295@gmail.com>
2026-07-24 21:26:30 +0530

    Implement distributed tracing with Micrometer and Zipkin

--- recommendation-service/src/main/java/com/axlero/recommendation/controller/RecommendationController.java
+++ recommendation-service/src/main/java/com/axlero/recommendation/controller/RecommendationController.java
@@ -1,5 +1,7 @@
 package com.axlero.recommendation.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,13 @@ import java.util.List;
 @RestController
 public class RecommendationController {
 
+    private static final Logger log =
+            LoggerFactory.getLogger(RecommendationController.class);
+
     @GetMapping("/recommendations")
-    public List<String> recommendations() {
+    public List<String> getRecommendations() {
+
+        log.info("========== Recommendation Service Called ==========");
 
         return List.of(
                 "Wireless Mouse",
