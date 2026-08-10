a2ff56625b2ea0bb298043405bc6cb87e4773c79
N Pradeepa <pradeepansp295@gmail.com>
2026-08-10 19:48:38 +0530

    Update gateway and recommendation resilience

--- recommendation-service/src/main/java/com/axlero/recommendation/controller/RecommendationController.java
+++ recommendation-service/src/main/java/com/axlero/recommendation/controller/RecommendationController.java
@@ -2,24 +2,60 @@ package com.axlero.recommendation.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/recommendations")
 public class RecommendationController {
 
     private static final Logger log =
             LoggerFactory.getLogger(RecommendationController.class);
 
-    @GetMapping("/recommendations")
+    private volatile boolean latencyEnabled = false;
+
+    // Enable Latency
+    @PostMapping("/trigger-latency")
+    public String enableLatency() {
+
+        latencyEnabled = true;
+
+        log.info("========== Latency Mode Enabled ==========");
+
+        return "Latency Enabled Successfully";
+    }
+
+    // Disable Latency
+    @PostMapping("/disable-latency")
+    public String disableLatency() {
+
+        latencyEnabled = false;
+
+        log.info("========== Latency Mode Disabled ==========");
+
+        return "Latency Disabled Successfully";
+    }
+
+    // Check Latency Status
+    @GetMapping("/latency-status")
+    public String latencyStatus() {
+
+        return latencyEnabled ? "Latency Enabled" : "Latency Disabled";
+    }
+
+    // Recommendation API
+    @GetMapping
     public List<String> getRecommendations() throws InterruptedException {
 
         log.info("========== Recommendation Service Called ==========");
 
-        // Simulate a slow backend service
-        Thread.sleep(10000);   // 10 seconds
+        if (latencyEnabled) {
+
+            log.info("Simulating 10 seconds latency...");
+
+            Thread.sleep(10000);
+        }
 
         return List.of(
                 "Wireless Mouse",
